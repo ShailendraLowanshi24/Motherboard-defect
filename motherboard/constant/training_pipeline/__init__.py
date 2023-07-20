461c58ca25a08e2779e573addfd9b3494ef58d2d
ShailendraLowanshi24 <shailendrabfab@gmail.com>
2023-07-20 18:41:11 +0530

    data validation added

--- motherboard/constant/training_pipeline/__init__.py
+++ motherboard/constant/training_pipeline/__init__.py
@@ -8,3 +8,16 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/17cX3dbYt9mTG7tY7kvJMNMCsVqF8nbfj/view?usp=sharing"
+
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
--- motherboard/pipeline/training_pipeline.py
+++ motherboard/pipeline/training_pipeline.py
@@ -2,17 +2,17 @@ import sys, os
 from motherboard.logger import logging
 from motherboard.exception import AppException
 from motherboard.components.data_ingestion import DataIngestion
-#from motherboard.components.data_validation import DataValidation
+from motherboard.components.data_validation import DataValidation
 #from motherboard.components.model_trainer import ModelTrainer
 
 
 from motherboard.entity.config_entity import (DataIngestionConfig,
-                                                 #DataValidationConfig,
+                                                 DataValidationConfig,
                                                  #ModelTrainerConfig
                                                  )
 
 from motherboard.entity.artifacts_entity import (DataIngestionArtifact,
-                                                    #DataValidationArtifact,
+                                                    DataValidationArtifact,
                                                     #ModelTrainerArtifact
                                                     )
 
@@ -20,7 +20,7 @@ from motherboard.entity.artifacts_entity import (DataIngestionArtifact,
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        #self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         #self.model_trainer_config = ModelTrainerConfig()
 
 
@@ -50,29 +50,29 @@ class TrainPipeline:
 
 
     
-    # def start_data_validation(
-    #     self, data_ingestion_artifact: DataIngestionArtifact
-    # ) -> DataValidationArtifact:
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
-    #     try:
-    #         data_validation = DataValidation(
-    #             data_ingestion_artifact=data_ingestion_artifact,
-    #             data_validation_config=self.data_validation_config,
-    #         )
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
 
-    #         data_validation_artifact = data_validation.initiate_data_validation()
+            data_validation_artifact = data_validation.initiate_data_validation()
 
-    #         logging.info("Performed the data validation operation")
+            logging.info("Performed the data validation operation")
 
-    #         logging.info(
-    #             "Exited the start_data_validation method of TrainPipeline class"
-    #         )
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
 
-    #         return data_validation_artifact
+            return data_validation_artifact
 
-    #     except Exception as e:
-    #         raise AppException(e, sys) from e
+        except Exception as e:
+            raise AppException(e, sys) from e
 
 
 
@@ -98,9 +98,9 @@ class TrainPipeline:
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(
-            #     data_ingestion_artifact=data_ingestion_artifact
-            # )
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
             # if data_validation_artifact.validation_status == True:
             #     model_trainer_artifact = self.start_model_trainer()
